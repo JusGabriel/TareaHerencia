d94e19a9ab99742e9aa96dbe9af60b83e024872c
Luxyz <elkin.diaz@epn.edu.ec>
2024-11-20 15:54:22 -0500

    Initial commit

---
+++ src/Ejercicio1/Animal.java
@@ -0,0 +1,38 @@
+public class Animal {
+    private String nombre;
+    private int edad;
+
+    Animal(String nombre, int edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public void hacerSonido() {
+        System.out.println("Sonido");
+    }
+
+    public static void main (String [] args) {
+        Perro p1 = new Perro("Bruno", 10);
+        System.out.println("Nombre: " + p1.getNombre()+"\nEdad: " + p1.getEdad());
+        p1.hacerSonido();
+        Gato g1 = new Gato("Bola de Nieve", 2);
+        System.out.println("Nombre: " + g1.getNombre()+"\nEdad: " + g1.getEdad());
+        g1.hacerSonido();
+
+    }
+}
---
+++ src/Ejercicio1/Perro.java
@@ -0,0 +1,11 @@
+public class Perro extends Animal {
+
+    Perro(String nombre, int edad){
+        super(nombre, edad);
+    }
+
+    @Override
+    public void hacerSonido() {
+        System.out.println("El perro ladra: Guau, guau!.");
+    }
+}

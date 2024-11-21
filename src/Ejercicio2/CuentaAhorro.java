60a3625a9f9c00795c4250716a1ba8ec22520d1a
JusGabriel <168014809+JusGabriel@users.noreply.github.com>
2024-11-20 19:04:49 -0500

    Update CuentaAhorro.java

--- src/Ejercicio2/CuentaAhorro.java
+++ src/Ejercicio2/CuentaAhorro.java
@@ -3,14 +3,13 @@ package Ejercicio2;
 public class CuentaAhorro extends CuentaBancaria{
     CuentaAhorro( Cliente cliente, String numeroCuenta){
         super(cliente, numeroCuenta, 500);
-
-
     }
 
     @Override
     public double retirar(double monto) {
         if (this.getSaldo()<100) {
             System.out.println("Usted no puede realizar retiros.");
+            return this.getSaldo();
         }
         return super.retirar(monto);
     }
